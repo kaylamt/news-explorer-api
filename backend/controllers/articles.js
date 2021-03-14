5830e79aa698f03711c16938327447a0f692435a
Kayla Thomas <kthomas2478@gmail.com>
2021-03-14 18:39:12 -0400

    controllers start

--- backend/controllers/articles.js
+++ backend/controllers/articles.js
@@ -1,1 +1,41 @@
 const Article = require('../models/article');
+
+module.exports.getArticles = (_req, res, next) => {
+  Article.find({})
+    .then((article) => res.send(article))
+    .catch(next);
+};
+
+module.exports.createArticle = (req, res, next) => {
+  const {
+    title,
+    text,
+    date,
+    source,
+    link,
+    image,
+  } = req.body;
+  const owner = req.user._id;
+
+  Article.create({
+    title,
+    text,
+    date,
+    source,
+    link,
+    image,
+    owner,
+  })
+    .then((article) => res.send(article))
+    .catch(next);
+};
+
+module.exports.deleteArticle = (req, res, next) => {
+  Article.findOneAndRemove({ _id: req.params.id, owner: req.user._id })
+    .then((article) => {
+      if (article) {
+        return res.send(article);
+      } throw new NotFoundError('User not found');
+    })
+    .catch(next);
+};
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,1 +1,7 @@
 const User = require('../models/user');
+
+module.exports.getUsers = (_req, res, next) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch(next);
+};
